6a7b61c0ce24cbc4ec758e8d07db3dc985db95c5
Patricio Ezequiel Hondagneu Roig <5291103+patricio-ezequiel-hondagneu-roig@users.noreply.github.com>
2022-05-29 23:05:44 -0300

    Change Game component to functional.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 import { calculateWinnerInformation } from "../shared/calculateWinnerInformation.function";
 import { convertSquareValueToLabel } from "../shared/convertSquareValueToLabel.function";
@@ -13,24 +13,21 @@ interface GameState {
     xIsNext: boolean;
 }
 
-export class Game extends React.Component<{}, GameState> {
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            boardHistory: [{
-                squares: [
-                    SquareValue.None, SquareValue.None, SquareValue.None,
-                    SquareValue.None, SquareValue.None, SquareValue.None,
-                    SquareValue.None, SquareValue.None, SquareValue.None,
-                ]
-            }],
-            currentMoveNumber: 0,
-            xIsNext: true
-        };
-    }
+export const Game = (): JSX.Element | null => {
+    const [state, setState] = useState<GameState>({
+        boardHistory: [{
+            squares: [
+                SquareValue.None, SquareValue.None, SquareValue.None,
+                SquareValue.None, SquareValue.None, SquareValue.None,
+                SquareValue.None, SquareValue.None, SquareValue.None,
+            ]
+        }],
+        currentMoveNumber: 0,
+        xIsNext: true
+    });
 
-    handleClick = (index: number) => {
-        const boardHistory = this.state.boardHistory.slice(0, this.state.currentMoveNumber + 1);
+    const handleClick = (index: number) => {
+        const boardHistory = state.boardHistory.slice(0, state.currentMoveNumber + 1);
         const currentBoard = boardHistory[boardHistory.length - 1];
 
         if (calculateWinnerInformation(currentBoard.squares).winner !== SquareValue.None || currentBoard.squares[index] !== SquareValue.None) {
@@ -38,72 +35,72 @@ export class Game extends React.Component<{}, GameState> {
         }
 
         const updatedSquares = [...currentBoard.squares];
-        updatedSquares[index] = this.state.xIsNext ? SquareValue.X : SquareValue.O;
+        updatedSquares[index] = state.xIsNext ? SquareValue.X : SquareValue.O;
 
-        this.setState({
+        setState({
             boardHistory: [...boardHistory, { squares: updatedSquares }],
             currentMoveNumber: boardHistory.length,
-            xIsNext: !this.state.xIsNext,
+            xIsNext: !state.xIsNext,
         });
     };
 
-    jumpTo = (moveNumber: number) => {
-        this.setState({
+    const jumpTo = (moveNumber: number) => {
+        setState({
+            ...state,
             currentMoveNumber: moveNumber,
             xIsNext: (moveNumber % 2) === 0,
         });
     };
 
-    render = (): JSX.Element => {
-        const boardHistory = this.state.boardHistory;
-        const currentBoard = boardHistory[this.state.currentMoveNumber];
-        const winnerInformation = calculateWinnerInformation(currentBoard.squares);
+    const boardHistory = state.boardHistory;
+    const currentBoard = boardHistory[state.currentMoveNumber];
+    const winnerInformation = calculateWinnerInformation(currentBoard.squares);
 
-        const moves = boardHistory.map((board, moveNumber) => {
-            const label = moveNumber === 0
-                ? `Go to game start`
-                : `Go to move #${moveNumber}`;
-
-            return (
-                <li key={moveNumber}>
-                    <button onClick={() => { this.jumpTo(moveNumber); }}>
-                        {label}
-                    </button>
-                </li>
-            );
-        });
-
-        let highlightedSquareIndexes: number[] = [];
-        let status: string;
-
-        if (winnerInformation.winner !== SquareValue.None) {
-            highlightedSquareIndexes = [
-                ...highlightedSquareIndexes,
-                ...winnerInformation.winningLineIndexes
-            ];
-            const winnerLabel = convertSquareValueToLabel(winnerInformation.winner);
-            status = `Winner: ${winnerLabel}`;
-        }
-        else {
-            status = currentBoard.squares.includes(SquareValue.None)
-                ? `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
-                : `Draw`;
-        }
+    const moves = boardHistory.map((board, moveNumber) => {
+        const label = moveNumber === 0
+            ? `Go to game start`
+            : `Go to move #${moveNumber}`;
 
         return (
-            <div className="game">
-                <div className="game-board">
-                    <Board
-                        squares={currentBoard.squares}
-                        highlightedSquareIndexes={highlightedSquareIndexes}
-                        onClick={(index) => { this.handleClick(index); }}
-                    />
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
-                </div>
-            </div>
+            <li key={moveNumber}>
+                <button onClick={() => { jumpTo(moveNumber); }}>
+                    {label}
+                </button>
+            </li>
         );
-    };
-}
+    });
+
+    let highlightedSquareIndexes: number[] = [];
+    let status: string;
+
+    if (winnerInformation.winner !== SquareValue.None) {
+        highlightedSquareIndexes = [
+            ...highlightedSquareIndexes,
+            ...winnerInformation.winningLineIndexes
+        ];
+        const winnerLabel = convertSquareValueToLabel(winnerInformation.winner);
+        status = `Winner: ${winnerLabel}`;
+    }
+    else {
+        status = currentBoard.squares.includes(SquareValue.None)
+            ? `Next player: ${state.xIsNext ? 'X' : 'O'}`
+            : `Draw`;
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={currentBoard.squares}
+                    highlightedSquareIndexes={highlightedSquareIndexes}
+                    onClick={(index) => { handleClick(index); }}
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+
+};
