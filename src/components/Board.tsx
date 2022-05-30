81195fd24f40c1da0d70367f6bf2e4bef93ab9c4
Patricio Ezequiel Hondagneu Roig <5291103+patricio-ezequiel-hondagneu-roig@users.noreply.github.com>
2022-05-30 17:30:15 -0300

    Refactored board to iterate over rows and squares.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,31 +10,40 @@ interface BoardProps {
 const UnmemoizedBoard = (props: BoardProps): JSX.Element | null => {
     const renderSquare = (index: number): JSX.Element => {
         return <Square
+            key={index}
             value={props.boardInformation.squares[index]}
             isHighlighted={props.boardInformation.valueAtIndexIsInWinningLine(index)}
             onClick={() => { props.onClick(index); }}
         />;
     };
 
-    return (
-        <div>
-            <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </div>
-            <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div>
-        </div>
-    );
+    const renderRow = (startIndex: number, endIndex: number): JSX.Element => {
+        const squares: JSX.Element[] = [];
+        for (let index = startIndex; index < endIndex; index++) {
+            const square = renderSquare(index);
+            squares.push(square);
+        }
+
+        return <div className="board-row" key={startIndex}>
+            {squares}
+        </div>;
+    };
+
+    const renderBoard = (boardHeight: number, boardWidth: number): JSX.Element => {
+        const rows: JSX.Element[] = [];
+        for (let rowIndex = 0; rowIndex < boardHeight; rowIndex++) {
+            const startIndex = rowIndex * boardWidth;
+            const endIndex = startIndex + boardWidth;
+            const row = renderRow(startIndex, endIndex);
+            rows.push(row);
+        }
+
+        return <div>
+            {rows}
+        </div>;
+    };
+
+    return renderBoard(3, 3)
 };
 
 export const Board = React.memo(UnmemoizedBoard);
